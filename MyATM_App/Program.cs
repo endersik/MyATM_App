21643580cf4def7fe63a6ad485509fd83a750815
Ender Şık <mailtoender@gmail.com>
2024-04-16 18:05:27 +0300

    Project is launched with fundamental UI elements.

---
+++ MyATM_App/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Title = "My ATM App";
+
+Console.WriteLine("\n\n------------------Welcome to My ATM App------------------");
+Console.WriteLine("\n\nPress enter to continue...\n");
+Console.ReadLine();
